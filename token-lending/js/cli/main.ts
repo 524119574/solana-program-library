8dc5c534dbdd85223a212201b76b9479e9a1e331
Wang Ge <gw980531@gmail.com>
2021-05-12 18:57:59 +0800

    changed to test net in js script.

--- token-lending/js/cli/main.ts
+++ token-lending/js/cli/main.ts
@@ -2,13 +2,13 @@
  * Exercises the token-lending program
  */
 
-import { createLendingMarket, deployPorgram } from "./token-lending-test";
+import { deployPorgram } from "./token-lending-test";
 
 async function main() {
   // These test cases are designed to run sequentially and in the following order
   console.log("Run test: createLendingMarket");
   await deployPorgram();
-  await createLendingMarket();
+//   await createLendingMarket();
   console.log("Success\n");
 }
 
--- token-lending/js/cli/token-lending-test.ts
+++ token-lending/js/cli/token-lending-test.ts
@@ -4,9 +4,9 @@
 import { Account, Connection } from "@solana/web3.js";
 import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
-import { LENDING_PROGRAM_ID, LendingMarket } from "../client";
-import { newAccountWithLamports } from "../client/util/new-account-with-lamports";
-import { url } from "../client/util/url";
+// import { LENDING_PROGRAM_ID, LendingMarket } from "../client";
+// import { newAccountWithLamports } from "../client/util/new-account-with-lamports";
+// import { url } from "../client/util/url";
 import {
   BPFLoader,
   Wallet,
@@ -17,48 +17,51 @@ let connection: Connection | undefined;
 async function getConnection(): Promise<Connection> {
   if (connection) return connection;
 
-  connection = new Connection("https://devnet.solana.com", "recent");
+  connection = new Connection("https://testnet.solana.com", "recent");
   const version = await connection.getVersion();
 
-  console.log("Connection to cluster established:", url, version);
+//   console.log("Connection to cluster established:", url, version);
   return connection;
 }
 
-export async function createLendingMarket(): Promise<void> {
-  const connection = await getConnection();
-
-  const payer = await newAccountWithLamports(
-    connection,
-    100000000000 /* wag */
-  );
-
-  console.log("creating quote token mint");
-  const quoteMintAuthority = new Account();
-  const quoteTokenMint = await Token.createMint(
-    connection,
-    payer,
-    quoteMintAuthority.publicKey,
-    null,
-    2,
-    TOKEN_PROGRAM_ID
-  );
-
-  const lendingMarketAccount = new Account();
-  await LendingMarket.create({
-    connection,
-    tokenProgramId: TOKEN_PROGRAM_ID,
-    lendingProgramId: LENDING_PROGRAM_ID,
-    quoteTokenMint: quoteTokenMint.publicKey,
-    lendingMarketAccount,
-    lendingMarketOwner: payer.publicKey,
-    payer,
-  });
-}
+// export async function createLendingMarket(): Promise<void> {
+//   const connection = await getConnection();
+//
+//   const payer = await newAccountWithLamports(
+//     connection,
+//     100000000000 /* wag */
+//   );
+//
+//   console.log("creating quote token mint");
+//   const quoteMintAuthority = new Account();
+//   const quoteTokenMint = await Token.createMint(
+//     connection,
+//     payer,
+//     quoteMintAuthority.publicKey,
+//     null,
+//     2,
+//     TOKEN_PROGRAM_ID
+//   );
+//
+//   const lendingMarketAccount = new Account();
+//   await LendingMarket.create({
+//     connection,
+//     tokenProgramId: TOKEN_PROGRAM_ID,
+//     lendingProgramId: LENDING_PROGRAM_ID,
+//     quoteTokenMint: quoteTokenMint.publicKey,
+//     lendingMarketAccount,
+//     lendingMarketOwner: payer.publicKey,
+//     payer,
+//   });
+// }
 
 export async function deployPorgram(): Promise<void> {
   const conn = await getConnection();
-  const programBinary = readFileSync(process.env.TOKEN_LENDING_SO_FILE_PATH!)
-  const wallet = await Wallet.fromMnemonic(process.env.WALLET_MNEMONIC!, conn);
+//   const programBinary = readFileSync(process.env.TOKEN_LENDING_SO_FILE_PATH!)
+//   const wallet = await Wallet.fromMnemonic(process.env.WALLET_MNEMONIC!, conn);
+  const programBinary = readFileSync("/Users/wangge/Projects/rust/solana-program-library/target/deploy/spl_token_lending.so")
+  const wallet = await Wallet.fromMnemonic("small wire setup start wave couple riot ordinary rifle vessel frozen goose", conn);
+
   console.log(`deploying token lending...`)
   const bpfLoader = new BPFLoader(wallet)
 
